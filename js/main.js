2f59a1c3811d09deb3b4f3838ae04ed0da48443c
Zach Bauer <zbauer91@gmail.com>
2017-10-21 19:10:29 -0400

    Added objects and some more logic

--- js/main.js
+++ js/main.js
@@ -2,23 +2,51 @@
 
 console.log("Up and running!");
 
-var cards = ["queen", "king","queen","king"];
+var cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
+}];
+
+
+
+
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
 
-console.log("User Flipped " + cardOne);
-
-var cardTwo = cards[1];
-cardsInPlay.push(cardTwo);
-
-console.log("User Flipped " + cardTwo);
-
-if (cardsInPlay.length === 2){
+function checkForMatch(){
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 		alert("You found a match!");
 	} else {
 		alert("Sorry, try again!");
 	}
 }
+
+
+function flipCard(cardID) {
+	console.log(cards[cardID].rank + " of " + cards[cardID].suit + " was flipped");
+	cardsInPlay.push(cards[cardID].rank);
+	if (cardsInPlay.length === 2){
+		checkForMatch();
+	}
+}
+
+
+
+flipCard(1);
