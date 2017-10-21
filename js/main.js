b88d6a5667f07efdcfed107668bb046f96df0647
Zach Bauer <zbauer91@gmail.com>
2017-10-21 10:20:30 -0400

    Added in some text logic for remembering which cards are in play.

--- js/main.js
+++ js/main.js
@@ -2,7 +2,23 @@
 
 console.log("Up and running!");
 
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
+var cards = ["queen", "king","queen","king"];
+var cardsInPlay = [];
+
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+
+console.log("User Flipped " + cardOne);
+
+var cardTwo = cards[1];
+cardsInPlay.push(cardTwo);
+
+console.log("User Flipped " + cardTwo);
+
+if (cardsInPlay.length === 2){
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again!");
+	}
+}
