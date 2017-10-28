71a8712296ea874532673800183e22b6b0e9df9b
Zach Bauer <bunbun@Zachs-MacBook-Pro.local>
2017-10-28 13:16:03 -0400

    Added logic for new reset button, which wipes the board without refreshing page. Added logic to determine user score and use it to alert the user when they've won the game. Aded logic to remove event listneer on cards that have already been clicked, disallowing user to click already flipped cards. Added logic to turn cards back over when a match is not found.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 
 
 console.log("Up and running!");
-
 var cards = [
 {
 	rank: "queen",
@@ -24,26 +23,40 @@ var cards = [
 	cardImage: "images/king-of-diamonds.png"
 }];
 
-
-
-
+var scoreBoard = document.getElementById('score');
+var score = 0;
 var cardsInPlay = [];
 
-
 function checkForMatch(){
-	if (cardsInPlay[0] === cardsInPlay[1]) {
+	var itemOne = '[data-id="' + cardsInPlay[0].ID + '"]';
+	var itemTwo = '[data-id="' + cardsInPlay[1].ID + '"]';
+	if (cardsInPlay[0].rank === cardsInPlay[1].rank) {
 		alert("You found a match!");
+		score += 2;
+		scoreBoard.innerHTML=("Current Score = " + score);
 	} else {
 		alert("Sorry, try again!");
-		//add logic here to revert back to img.back, remove cards from this array
+		document.querySelector(itemOne).setAttribute('src','images/back.png');
+		document.querySelector(itemTwo).setAttribute('src','images/back.png');
+		document.querySelector(itemOne).addEventListener("click", flipCard);
+		document.querySelector(itemTwo).addEventListener("click", flipCard);
 	}
+	if (score === cards.length){
+		alert("You won!");
+	}
+	cardsInPlay.pop();
+	cardsInPlay.pop();
 }
 
 
 function flipCard() {
+	this.removeEventListener("click", flipCard);
 	var cardId = this.getAttribute('data-id');
 	console.log(cards[cardId].rank + " of " + cards[cardId].suit + " was flipped");
-	cardsInPlay.push(cards[cardId].rank);
+	cardsInPlay.push({
+		rank: cards[cardId].rank,
+		ID: cardId
+		});
 	this.setAttribute('src', cards[cardId].cardImage);
 	if (cardsInPlay.length === 2){
 		checkForMatch();
@@ -55,10 +68,23 @@ function createBoard() {
 		var cardElement = document.createElement('img');
 		cardElement.setAttribute('src','images/back.png');
 		cardElement.setAttribute('data-id', i);
+		cardElement.setAttribute('class', 'boardCard');
 		cardElement.addEventListener('click', flipCard);
 		document.getElementById('gameboard').appendChild(cardElement);
-
+		document.getElementById('resetButton').addEventListener('click', resetBoard);
 	} 
+}
+
+function resetBoard(){
+	score = 0;
+	scoreBoard.innerHTML=("Current Score = " + score);
+	cardsInPlay = [];
+	var myNode = document.getElementById("gameboard");
+	while (myNode.firstChild) {
+    	myNode.removeChild(myNode.firstChild);
+	}
+	createBoard();
+
 }
 
 createBoard();
